570dc845aba3d6a3c913ce0745cfbd6626d7f155
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-26 09:55:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function logDriverNames(array){
+    array.forEach(function(elment){
+       console.log(elment.name);
+  });
+};
+function logDriversByHometown(array,homeTown){
+  array.forEach(function(elment){
+    if(elment.hometown === homeTown){
+      console.log(elment.name);
+    }
+  });
+};
+function driversByRevenue(array){
+  const newArray = [...array].sort(function (firstDriver, secondDriver){
+    return firstDriver.revenue  - secondDriver.revenue ;
+  });
+  return newArray;
+};
+function driversByName(array){
+  const newArray = [...array].sort(function(firstDriver, secondDriver){
+    return firstDriver.name.localeCompare(secondDriver.name);
+  });
+  return newArray;
+};
+function totalRevenue(array){
+  return array.reduce(function(sum,elment){
+    return elment.revenue + sum;
+  },0);
+};
+function averageRevenue(array){
+  return totalRevenue(array) / array.length;
+};
