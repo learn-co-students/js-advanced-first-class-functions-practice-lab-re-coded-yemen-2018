50b0a7af46863d35cfab8fd3565ed9d22c0babde
Salah Al-Dhaferi <binaryminded@gmail.com>
2018-08-30 08:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Code your solution in this file!
+function logDriverNames(array){
+  array.forEach(function (element){
+    console.log(element.name);
+  })
+}
+
+function logDriversByHometown(array, arg){
+  array.forEach(function (element){
+    if (element.hometown === arg){
+      console.log(element.name);
+    }
+  })
+  
+}
+
+function driversByRevenue(array){
+  let newArray = [];
+  
+  array.forEach(function (element){
+    newArray.push(element)
+  })
+  
+  newArray.sort(function (a,b){
+    return a.revenue - b.revenue;
+  });
+  return newArray;
+    
+}
+
+function driversByName(array){
+  let newArray = [];
+  
+  array.forEach(function (element){
+    newArray.push(element)
+  })
+  
+  newArray.sort(function (a,b){
+    return a.name.localeCompare(b.name);
+  });
+  return newArray;
+    
+}
+
+function totalRevenue(array){
+  const totalRev = function(agg, element){
+    return agg + element.revenue;
+    
+  };
+  return array.reduce(totalRev,0);
+  
+  
+}
+
+function averageRevenue(array){
+  let count = array.length;
+  const totalRev = function(agg, element,i){
+    return (agg + element.revenue);
+    
+  };
+  return array.reduce(totalRev,0)/count;
+  
+
+}
+
+
+
+
+
+
+
+
